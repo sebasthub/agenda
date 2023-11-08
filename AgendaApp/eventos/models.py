e6809a5084377dcf0e9735b414e617f7c19499df
sebastiaonogueira <sebastiaonogueira@unitins.br>
2023-11-08 20:52:46 -0300

    toString do evento

--- AgendaApp/eventos/models.py
+++ AgendaApp/eventos/models.py
@@ -5,3 +5,8 @@ class Evento(models.Model):
     titulo_do_evento = models.CharField(max_length=500)
     data_e_hora = models.DateTimeField(auto_now=False, auto_now_add=False)
     descricao = models.CharField(max_length=50)
+    def __str__(self) -> str:
+        return self.titulo_do_evento
+        
+        
+    
