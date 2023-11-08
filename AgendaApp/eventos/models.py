38ed94621e88fc05ed27dc9e6f5033fa1958fe04
sebastiaonogueira <sebastiaonogueira@unitins.br>
2023-11-08 20:35:18 -0300

    criacao do model de evento

--- AgendaApp/eventos/models.py
+++ AgendaApp/eventos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Evento(models.Model):
+    titulo_do_evento = models.CharField(max_length=500)
+    data_e_hora = models.DateTimeField(auto_now=False, auto_now_add=False)
+    descricao = models.CharField(max_length=50)
