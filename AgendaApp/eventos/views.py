7363a6768cabc8394d23c22966eea874db1d70cf
sebastiaonogueira <sebastiaonogueira@unitins.br>
2023-11-09 08:00:36 -0300

    views e templates que listam eventos

--- AgendaApp/eventos/views.py
+++ AgendaApp/eventos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Evento
 
 # Create your views here.
+def index(request):
+    eventos = Evento.objects.all()
+    contexto = {
+        "eventos": eventos,
+    }
+    return render(request=request, template_name='index.html',context=contexto)
